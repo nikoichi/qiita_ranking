0f8a7d35e76ec764b2c8ce8084633e950471f9f8
Koichi Watanabe <monopori151@gmail.com>
2017-06-23 15:39:46 +0900

    アソシエイトが無くてもsaveできるように設定変更

--- app/models/qiita_user.rb
+++ app/models/qiita_user.rb
@@ -1,4 +1,4 @@
 class QiitaUser < ApplicationRecord
-  belongs_to :organization
+  belongs_to :organization, optional: true
   has_many :stocks, through: :qiita_user_stocks, class_name: 'ITem', foreign_key: 'item_id'
 end
