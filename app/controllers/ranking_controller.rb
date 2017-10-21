43847c43f508c39fd1762f7c2a86250bda9731c0
Koichi Watanabe <monopori151@gmail.com>
2017-10-21 22:48:09 +0900

    リソース作成

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,4 +1,6 @@
 class RankingController < ApplicationController
   def show
+    tag_id = params[:id] || QiitaTag::RUBY_ID
+    @items = QiitaTag.find(tag_id).items.where(qiita_user_id: 2)
   end
 end
