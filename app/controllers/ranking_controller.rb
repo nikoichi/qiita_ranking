48ce03bc0cb87e295335fe8edb8a6cd43d3d56f5
Koichi Watanabe <monopori151@gmail.com>
2017-10-24 22:51:44 +0900

    インスタンスメソッド内容変更

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,6 +1,6 @@
 class RankingController < ApplicationController
   def show
     tag_id = params[:id] || QiitaTag::RUBY_ID
-    @items = QiitaTag.find(tag_id).items.where(qiita_user_id: 2)
+    @items = QiitaTag.find(tag_id).items.order(:likes_count).limit(10)
   end
 end
