3bdde2a553a629a97485e5a2d05dd9cdd5cc454b
Koichi Watanabe <monopori151@gmail.com>
2017-11-06 22:39:25 +0900

    showじゃなくてindexだなって思ったので修正

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,6 +1,15 @@
 class RankingController < ApplicationController
-  def show
+  def index
     tag_id = params[:id] || QiitaTag::RUBY_ID
     @items = QiitaTag.find(tag_id).items.order(:likes_count).limit(10)
+
+    # item_search = ItemSearch.new(item_search_params)
+    # @items = item_search.search_items
+  end
+
+  private
+
+  def item_search_params
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'ranking#show'
-  resources :ranking, only: :show
+  root to: 'ranking#index'
+  resources :ranking, only: :index
 end
