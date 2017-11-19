e8e01afbca5b95655c098a6246c0e7543d9e8182
Koichi Watanabe <monopori151@gmail.com>
2017-11-19 19:17:41 +0900

    seed追加

---
+++ db/fixtures/masters/02_category.rb
@@ -0,0 +1,4 @@
+Category.seed do |s|
+  s.id = 1
+  s.name = '言語'
+end
---
+++ db/fixtures/masters/03_category_tag.rb
@@ -0,0 +1,10 @@
+CategoryTag.seed do |s|
+  s.id = 1
+  s.category_id = 1
+  s.tag_id = 2
+end
+CategoryTag.seed do |s|
+  s.id = 2
+  s.category_id = 1
+  s.tag_id = 3
+end
