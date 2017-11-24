f1e1f63d627cfb8806dc17679d7f2705533ca78f
Koichi Watanabe <monopori151@gmail.com>
2017-11-24 22:00:34 +0900

    いいね数、ストック数をitemsテーブルに持たせる

--- spec/factories/like_total_counts.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :like_total_count do
-    like_total_count 1
-    item nil
-  end
-end
--- spec/factories/stock_total_counts.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :stock_total_count do
-    stock_total_count 1
-    item nil
-  end
-end
