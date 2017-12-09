dc66ad24efaa534e606f3f1a6dc55718fb43ac6a
Koichi Watanabe <monopori151@gmail.com>
2017-12-09 22:12:08 +0900

    capistrano修正2

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,8 +7,8 @@ timeout 30
 listen "/tmp/unicorn.sock", :backlog => 64
 pid "/var/www/qiita_ranking/shared/tmp/pids/unicorn.pid"
 
-stderr_path "#{@app_path}/log/unicorn.stderr.log"
-stdout_path "#{@app_path}/log/unicorn.stdout.log"
+stderr_path "#{@app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{@app_path}/shared/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
   ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', ENV['RAILS_ROOT'])
